dd1601d68a598d5e86ca4beb0a8ff1603db963fd
Hitesh mishra <mishrahitesh854@gmail.com>
2023-12-05 14:30:46 +0530

    keyy

--- index.js
+++ index.js
@@ -392,8 +392,8 @@ app.post("/create-checkout-session", authenticateJwt, async (req, res) => {
       payment_method_types: ["card"],
       line_items,
       mode: "payment",
-      success_url: `https://wireview-frontend-p335kthr4-hiteshmishra2103s-projects.vercel.app//success`,
-      cancel_url: `https://wireview-frontend-p335kthr4-hiteshmishra2103s-projects.vercel.app//cancel`,
+      success_url: `https://wireview-frontend1.vercel.app/success`,
+      cancel_url: `https://wireview-frontend1.vercel.app/cancel`,
     });
 
     const newOrder = new Orders({
