11c94705482491af597550ebccf74c13032028a2
Hitesh Mishra <66027081+hiteshmishra2103@users.noreply.github.com>
2024-06-04 12:27:48 +0000

    mo

--- routes/stripeCheckoutRoutes.js
+++ routes/stripeCheckoutRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const User = require("../models/User");
+const User = require("../models/Users");
 const { stripeCheckout } = require("../controllers/stripeCheckoutController");
 
 router.post("/", stripeCheckout);
