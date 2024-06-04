48e4505f3cd3f5f72c8cf3c153c554be5b2bc564
Hitesh Mishra <66027081+hiteshmishra2103@users.noreply.github.com>
2024-06-04 13:19:21 +0000

    mo

--- routes/stripeCheckoutRoutes.js
+++ routes/stripeCheckoutRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const User = require("../models/Users");
 const { stripeCheckout } = require("../controllers/stripeCheckoutController");
+const { authenticateJwt } = require("../middleware/auth");
 
-router.post("/", stripeCheckout);
+router.post("/", authenticateJwt, stripeCheckout);
 module.exports = router;
